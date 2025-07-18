4be9be465fd7b48ee2edff89a42e4fa134fddc66
FreakQnZ <anuraag.srivatsa123@gmail.com>
2025-07-18 22:46:31 +0530

    cors updation to socket

--- server/utils/socketSetup.js
+++ server/utils/socketSetup.js
@@ -1,33 +1,32 @@
 import { Server } from "socket.io";
 
 const configureSocket = (server) => {
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:5173",
-            methods: ["GET", "POST"],
-        },
+  const io = new Server(server, {
+    cors: {
+      origin: ["http://localhost:5173", "https://anonychat-black.vercel.app/"],
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (id) => {
+      socket.join(id);
+      console.log(`User with ID: ${socket.id} joined room: ${id}`);
     });
 
-    io.on("connection", (socket) => {
-        console.log(`User Connected: ${socket.id}`);
-
-        socket.on("join_room", (id) => {
-            socket.join(id);
-            console.log(`User with ID: ${socket.id} joined room: ${id}`);
-        })
-
-        socket.on("send_message", (data) => {
-            console.log(data);
-            socket.to(data.room).emit("receive_message", data)
-        })
-
-
-        socket.on("disconnect", () => {
-            console.log(`User Disconnected: ${socket.id}`);
-        });
+    socket.on("send_message", (data) => {
+      console.log(data);
+      socket.to(data.room).emit("receive_message", data);
     });
 
-    return io;
+    socket.on("disconnect", () => {
+      console.log(`User Disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
 };
 
 export default configureSocket;
