c24b98c5f7983214e4a104669b6293ad893caf82
FreakQnZ <anuraag.srivatsa123@gmail.com>
2025-07-19 22:33:19 +0530

    fixed socket error on render

--- server/utils/socketSetup.js
+++ server/utils/socketSetup.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const configureSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "https://anonychat-black.vercel.app/"],
+      origin: ["http://localhost:5173", "https://anonychat-black.vercel.app"],
       methods: ["GET", "POST"],
     },
   });
